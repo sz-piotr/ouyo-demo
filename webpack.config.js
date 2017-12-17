44a6caae7cf43e810c61118ab5e659aee36fa033
sz-piotr <szlachciak.piotr@gmail.com>
2017-12-17 10:03:47 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const isProduction = process.env.npm_lifecycle_event === 'build'
+
+const config = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'app.[chunkhash].js',
+    path: path.join(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }, {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: {
+          loader: 'style-loader',
+          options: { sourceMap: true }
+        },
+        use: [{
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }]
+      })
+    }]
+  },
+  devServer: {
+    stats: 'minimal',
+    contentBase: 'src/assets'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html')
+    }),
+    new ExtractTextPlugin('style.[contenthash].css'),
+  ]
+}
+
+if (isProduction) {
+  config.plugins = config.plugins.concat([
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new CopyWebpackPlugin([{ from: 'src/assets' }])
+  ])
+}
+
+module.exports = config
