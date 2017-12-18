62851f06084b70359b0e56009e348d69dc395656
sz-piotr <szlachciak.piotr@gmail.com>
2017-12-18 20:19:08 +0100

    add tilt input

--- src/game/input.js
+++ src/game/input.js
@@ -3,6 +3,8 @@ const KEY_RIGHT = 39
 const KEY_A = 65
 const KEY_D = 68
 
+const TILT_INSENSITIVITY = 5 // degrees
+
 const inputMap = {
   keys: {
     [KEY_LEFT]: false,
@@ -12,20 +14,36 @@ const inputMap = {
   },
   tilt: 0,
   get left () {
-    return this.keys[KEY_LEFT] || this.keys[KEY_A] || this.tilt < -1
+    return this.keys[KEY_LEFT] || this.keys[KEY_A] || this.tilt < -TILT_INSENSITIVITY
   },
   get right () {
-    return this.keys[KEY_RIGHT] || this.keys[KEY_D] || this.tilt > 1
+    return this.keys[KEY_RIGHT] || this.keys[KEY_D] || this.tilt > TILT_INSENSITIVITY
   }
 }
 
 window.addEventListener('keydown', function (e) {
   inputMap.keys[e.keyCode] = true
-})
+}, true)
 
 window.addEventListener('keyup', function (e) {
   inputMap.keys[e.keyCode] = false
-})
+}, true)
+
+window.addEventListener('deviceorientation', function (e) {
+  throttleMessage(e.alpha, e.beta, e.gamma)
+  inputMap.tilt = e.gamma
+}, true)
+
+let free = true
+function throttleMessage (...msgs) {
+  if (free) {
+    free = false
+    setTimeout(() => {
+      console.log(...msgs)
+      free = true
+    }, 1000)
+  }
+}
 
 export function getInputXAxis () {
   const left = inputMap.left
