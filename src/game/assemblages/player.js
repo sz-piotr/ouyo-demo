9e3bf960d98887f901335b1813c5c1ebe94482f5
sz-piotr <szlachciak.piotr@gmail.com>
2017-12-23 17:38:55 +0100

    start with 3 lives

--- src/game/assemblages/player.js
+++ src/game/assemblages/player.js
@@ -10,6 +10,6 @@ import { WIDTH, HEIGHT } from '../canvas'
 export const player = entity => entity
   .add(new Transform(WIDTH / 2, HEIGHT - 50, 45))
   .add(new Velocity(0, 0))
-  .add(new Player(1))
+  .add(new Player(3))
   .add(getSprite('player.png'))
   .add(new Cannon(0.2, 0, 450))
